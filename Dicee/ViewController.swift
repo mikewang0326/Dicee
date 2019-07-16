b6660adbdc81e2bbffa5a004cdd14f4bfcb95251
MikeWang <hi.from.mikewang@gmail.com>
2019-07-16 20:58:10 +1200

    implement basic functionality

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -9,6 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let diceArray: Array = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
+    
+    var randomDiceeIndexLeft: Int = 0
+    var randomDiceeIndexRight: Int = 0
+    
     @IBOutlet weak var diceImageViewLeft: UIImageView!
     @IBOutlet weak var diceImageViewRight: UIImageView!
    
@@ -18,6 +24,14 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        randomDiceeIndexLeft = Int.random(in: 0 ... 5)
+        randomDiceeIndexRight = Int.random(in: 0 ... 5)
+        
+        diceImageViewLeft.image = UIImage(named: diceArray[randomDiceeIndexLeft])
+        diceImageViewRight.image = UIImage(named: diceArray[randomDiceeIndexRight])
+        
+    }
+    
 }
 
