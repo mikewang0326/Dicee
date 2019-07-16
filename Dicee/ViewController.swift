1da7aff8cc8cca32fd084f28a404fd112521a33b
MikeWang <hi.from.mikewang@gmail.com>
2019-07-16 19:47:00 +1200

    init code

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -9,7 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var diceImageViewLeft: UIImageView!
+    @IBOutlet weak var diceImageViewRight: UIImageView!
+   
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
